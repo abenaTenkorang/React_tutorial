c9464b8cda48ceb5049dc035df3877efbe605a71
abenaTenkorang <abenagy16@gmail.com>
2023-03-27 10:35:31 +0000

    Refract app with routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navigation from './components/header';
+import Footer from './components/footer';
 import './App.css';
+import TodoPage from './Others/todo';
+import About from './Others/about';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <main className="content">
+        <Navigation />
+        <section>
+          <Routes>
+            <Route path="/" element={<TodoPage />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </section>
+      </main>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
